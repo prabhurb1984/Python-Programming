99cf618c8dbc8ee9a08cda3116497860b95e4278
prabhurb1984 <prabhu.rb@bldeacet.ac.in>
2018-07-18 11:29:27 +0530

    my first version

---
+++ cal.py
@@ -0,0 +1,7 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+import calendar
+print(calendar.month(17,7))
+print(calendar.month(2018,1))
---
+++ swap.py
@@ -0,0 +1,10 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+x=input("enter the value of x")
+y=input("enter the value of y")
+temp=x
+x=y
+y=temp
+print("the value of x after swapping:{}".format(x))
+print("the value of y after swapping:{}".format(y))
